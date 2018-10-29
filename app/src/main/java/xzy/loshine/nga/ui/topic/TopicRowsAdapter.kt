4132f00c3cafb65da234eecaf26cc834ebd6a62d
Loshine <loshine@famulei.com>
2018-10-29 19:08:34 +0800

    :robot: Fix topic details row height problems with html table.

--- app/src/main/java/xzy/loshine/nga/ui/topic/TopicRowsAdapter.kt
+++ app/src/main/java/xzy/loshine/nga/ui/topic/TopicRowsAdapter.kt
@@ -1,5 +1,6 @@
 package xzy.loshine.nga.ui.topic
 
+import android.util.Log
 import android.util.LruCache
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
@@ -18,7 +19,7 @@ class TopicRowsAdapter
 @Inject constructor() : BaseQuickAdapter<TopicRowUiModel, BaseViewHolder>(R.layout.recycler_item_topic_rows) {
 
     // 缓存 3 页 webView 的高度，解决有图片的时候闪动的问题
-    private val heightCache = LruCache<Int, Int>(60)
+    private val heightCache = LruCache<Int, Float>(60)
 
     override fun convert(helper: BaseViewHolder, item: TopicRowUiModel) {
         helper.setGone(R.id.subject, item.index == 0)
@@ -28,35 +29,40 @@ class TopicRowsAdapter
                 .setText(R.id.time, item.time)
                 .setText(R.id.index, "#${item.index}")
         val contentWebView = helper.getView<ContentWebView>(R.id.content)
-        contentWebView.setListener {
-            if (helper.adapterPosition > -1 && heightCache[helper.adapterPosition] == null && it != 0) {
-                heightCache.put(helper.adapterPosition, it)
+        contentWebView.removeListener()
+        if (heightCache[helper.adapterPosition] == null) {
+            contentWebView.setListener {
+                if (helper.adapterPosition > -1 && heightCache[helper.adapterPosition] == null && it != 0f) {
+                    heightCache.put(helper.adapterPosition, it)
+                }
             }
         }
+        val layoutParams = getDefaultConstraintLayoutParams()
         if (heightCache[item.pid] != null) {
-            contentWebView.layoutParams.height = heightCache[item.pid]!!
-        } else {
-            contentWebView.layoutParams = getDefaultConstraintLayoutParams()
+            layoutParams.height = heightCache[item.pid]!!.toInt()
         }
+        contentWebView.layoutParams = layoutParams
+
         val html = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />${item.content}<script type=\"text/javascript\" src=\"file:///android_asset/collapse_script.js\"></script>"
         contentWebView.loadDataWithBaseURL("file:///android_asset/", html,
                 "text/html", "utf-8", "")
-        val avatarView = helper.getView<ImageView>(R.id.avatar)
-        GlideApp.with(avatarView)
-                .load(item.avatar)
-                .placeholder(R.drawable.default_board_icon)
-                .circleCrop()
-                .into(avatarView)
+        helper.getView<ImageView>(R.id.avatar).let {
+            GlideApp.with(it)
+                    .load(item.avatar)
+                    .placeholder(R.drawable.default_board_icon)
+                    .circleCrop()
+                    .into(it)
+        }
     }
 
     private fun getDefaultConstraintLayoutParams(): ConstraintLayout.LayoutParams {
-        val layoutParams = ConstraintLayout.LayoutParams(0, WRAP_CONTENT)
-        layoutParams.endToEnd = PARENT_ID
-        layoutParams.startToStart = PARENT_ID
-        layoutParams.topToBottom = R.id.group
-        layoutParams.marginStart = SizeUtils.dp2px(16f)
-        layoutParams.marginEnd = SizeUtils.dp2px(16f)
-        layoutParams.topMargin = SizeUtils.dp2px(16f)
-        return layoutParams
+        return ConstraintLayout.LayoutParams(0, WRAP_CONTENT).apply {
+            endToEnd = PARENT_ID
+            startToStart = PARENT_ID
+            topToBottom = R.id.group
+            marginStart = SizeUtils.dp2px(16f)
+            marginEnd = SizeUtils.dp2px(16f)
+            topMargin = SizeUtils.dp2px(16f)
+        }
     }
 }
--- app/src/main/java/xzy/loshine/nga/widget/ContentWebView.kt
+++ app/src/main/java/xzy/loshine/nga/widget/ContentWebView.kt
@@ -4,16 +4,21 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
-import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
+
 class ContentWebView(context: Context, attrs: AttributeSet?) : WebView(context, attrs) {
 
-    private var listener: ((Int) -> Unit)? = null
+    private var webAppInterface: WebAppInterface? = null
 
-    fun setListener(listener: (Int) -> Unit) {
-        this.listener = listener
+    fun setListener(listener: (Float) -> Unit) {
+        webAppInterface?.listener = listener
+    }
+
+    fun removeListener() {
+        webAppInterface?.listener = null
     }
 
     constructor(context: Context) : this(context, null)
@@ -28,15 +33,25 @@ class ContentWebView(context: Context, attrs: AttributeSet?) : WebView(context, 
         isVerticalScrollBarEnabled = false
         setBackgroundColor(Color.TRANSPARENT)
 
+        webAppInterface = WebAppInterface()
+        webAppInterface?.let { addJavascriptInterface(it, "AndroidFunction") }
         webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
-                post {
-                    measure(0, 0)
-                    Log.i("zzz", "measuredHeight=$measuredHeight")
-                    listener?.invoke(measuredHeight)
-                }
+                loadUrl("javascript:AndroidFunction.resize(document.body.scrollHeight)")
             }
+        }
+    }
+
+    private inner class WebAppInterface {
+
+        var listener: ((Float) -> Unit)? = null
+
+        @JavascriptInterface
+        fun resize(height: Float) {
+            val webViewHeight = height * resources.displayMetrics.density
+            //webViewHeight is the actual height of the WebView in pixels as per device screen density
+            listener?.invoke(webViewHeight)
         }
     }
 }
